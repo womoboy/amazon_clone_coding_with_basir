a424175da17a118b5e00f79adcca451e2304cf12
reza2boyce <reza2boyce@gmail.com>
2022-11-10 01:42:37 +0330

    show product in home page

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slime Shirt',
+            slug: 'nike-slime-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 23,
+            countInStock: 12,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-slime-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 32,
+            countInStock: 3,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 5,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Nike Slime Pant',
+            slug: 'nike-slime-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 53,
+            countInStock: 6,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 7,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Adidas Slime Pant',
+            slug: 'adidas-slime-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 35,
+            countInStock: 14,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 14,
+            description: 'high quality shirt'
+        }
+    ]
+}
+
+export default data;
