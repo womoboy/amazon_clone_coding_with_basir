b65d205277b1a76a0ea8d1e1cd640a9c275947f4
reza2boyce <reza2boyce@gmail.com>
2022-11-15 15:33:19 +0330

    complete cart screen

--- README.md
+++ README.md
@@ -1,42 +1,43 @@
 # MERN AMAZONA
 
 # Lessons
+
 1. Introduction
 2. Install Tools
 3. Create React App
 4. Create Git Repository
 5. List Products
-    1. create products array
-    2. add product images 679 × 829
-    3. render products
-    4. style products
+   1. create products array
+   2. add product images 679 × 829
+   3. render products
+   4. style products
 6. Add routing
-    1. npm i react-router-dom
-    2. create route for home screen
-    3. create router for product screen
+   1. npm i react-router-dom
+   2. create route for home screen
+   3. create router for product screen
 7. Create Node.JS Server
-    1. run npm init in root folder
-    2. Update package.json set type: module
-    3. Add .js to imports
-    4. npm install express
-    5. create server.js
-    6. add start command as node backend/server.js
-    7. require express
-    8. create route for / return backend is ready
-    9. move product.js from frontend to backend
-    10. create route for /api/products
-    11. return products
-    12. run npm start
+   1. run npm init in root folder
+   2. Update package.json set type: module
+   3. Add .js to imports
+   4. npm install express
+   5. create server.js
+   6. add start command as node backend/server.js
+   7. require express
+   8. create route for / return backend is ready
+   9. move product.js from frontend to backend
+   10. create route for /api/products
+   11. return products
+   12. run npm start
 8. Fetch Product From Backend
-    1. set proxy in package.json
-    2. npm install axios
-    3. use state hook
-    4. use effect hook
-    5. use reducer hook
+   1. set proxy in package.json
+   2. npm install axios
+   3. use state hook
+   4. use effect hook
+   5. use reducer hook
 9. Manage State By Reducer Hook
-    1. define reducer
-    2. update fetch data
-    3. get state from usReducer
+   1. define reducer
+   2. update fetch data
+   3. get state from usReducer
 10. Add bootstrap UI Framework
     1. npm install react-bootstrap
     2. update App.js
@@ -64,3 +65,7 @@
     1. create 2 columns
     2. display items list
     3. create action column
+17. Complete Cart Screen
+    1. click handler for inc/dec item
+    2. click handler for remove item
+    3. click handler for checkout
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,19 +1,42 @@
-import { useContext } from "react";
-import { Store } from "../Store";
-import { Helmet } from "react-helmet-async";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import MessageBox from "../components/MessageBox";
-import ListGroup from "react-bootstrap/ListGroup";
-import Button from "react-bootstrap/Button";
+import { useContext } from 'react';
+import { Store } from '../Store';
+import { Helmet } from 'react-helmet-async';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import MessageBox from '../components/MessageBox';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const CartScreen = () => {
+  const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  const updateCartHandler = async (item, quantity) => {
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry, Product is out of stock');
+      return;
+    }
+
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+
+  const removeHandler = (item) => {
+    ctxDispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  };
+
+  const checkoutHandler = () => {
+    navigate('/signin?redirect=/shipping');
+  };
 
   return (
     <div>
@@ -37,15 +60,24 @@ const CartScreen = () => {
                         src={item.image}
                         alt={item.name}
                         className="img-fluid rounded img-thumbnail"
-                      />{" "}
+                      />{' '}
                       <Link to={`/product/${item.slug}`}>{item.name}</Link>
                     </Col>
                     <Col md={3}>
-                      <Button variant="light" disabled={item.quantity === 1}>
-                        <i className="fas fa-minus-circle"></i>
-                      </Button>{" "}
-                      <span>{item.quantity}</span>{" "}
                       <Button
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity - 1)
+                        }
+                        variant="light"
+                        disabled={item.quantity === 1}
+                      >
+                        <i className="fas fa-minus-circle"></i>
+                      </Button>{' '}
+                      <span>{item.quantity}</span>{' '}
+                      <Button
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity + 1)
+                        }
                         variant="light"
                         disabled={item.quantity === item.countInStock}
                       >
@@ -54,7 +86,10 @@ const CartScreen = () => {
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant="light">
+                      <Button
+                        onClick={() => removeHandler(item)}
+                        variant="light"
+                      >
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
@@ -70,7 +105,7 @@ const CartScreen = () => {
               <ListGroup variant="flush">
                 <ListGroup.Item>
                   <h3>
-                    subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{" "}
+                    subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
                     items) : $
                     {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
                   </h3>
@@ -80,6 +115,7 @@ const CartScreen = () => {
                     <Button
                       type="button"
                       variant="primary"
+                      onClick={() => checkoutHandler()}
                       disabled={cartItems.length === 0}
                     >
                       Proceed to Checkout
