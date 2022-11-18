1acd8db08ef71b69abed8de708fff745efc07848
reza2boyce <reza2boyce@gmail.com>
2022-11-18 20:27:29 +0330

    create sample user

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
+  //add products
   await Product.remove({});
-  const createProducts = await Product.insertMany(data.products);
-  res.send({ createProducts });
+  const createdProducts = await Product.insertMany(data.products);
+
+  //add users
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+
+  res.send({ createdProducts, createdUsers });
 });
 
 export default seedRouter;
